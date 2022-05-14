f8d66effd023aa47c206964370a0238912096ef2
Obasoro Olakunle <obasorokunle@gmail.com>
2022-05-14 02:58:09 +0100

    update

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,14 @@
+import sys
+
 import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
 
 
 # some fancy stuff with pandas
 
-print("Job finished Successful")
+#print("Job finished Successful")
+
+print(f"Job finished Successful on day = f{day}")
